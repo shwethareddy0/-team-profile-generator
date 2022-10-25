1d0bb2e345e10b697a13795a53def50cda1eda62
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-10-24 22:14:04 -0700

    add employee unit tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,20 @@
+const Employee = require("../lib/Employee");
+
+const testEmployee = new Employee("temp employee", 5, "abc@gmail.com");
+
+describe("Employee test", () => {
+  it("returns Employee name", () => {
+    expect(testEmployee.getName()).toBe("temp employee");
+  });
+
+  it("returns Employee id", () => {
+    expect(testEmployee.getId()).toBe(5);
+  });
+
+  it("returns Employee email", () => {
+    expect(testEmployee.getEmail()).toBe("abc@gmail.com");
+  });
+  it("returns Employee role", () => {
+    expect(testEmployee.getRole()).toBe("Employee");
+  });
+});
